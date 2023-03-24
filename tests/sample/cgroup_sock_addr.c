44e31d202fe98e2229661731bfa0b2c1dd100f00
Anurag Saxena <anusa@microsoft.com>
2023-03-23 21:47:44 -0700

    fix

--- tests/sample/cgroup_sock_addr.c
+++ tests/sample/cgroup_sock_addr.c
@@ -11,54 +11,79 @@
 // Example:
 // .\scripts\generate_expected_bpf2c_output.ps1 .\x64\Debug\
 
+#include "bpf_endian.h"
 #include "bpf_helpers.h"
 #include "socket_tests_common.h"
 
 SEC("maps")
-struct bpf_map_def ingress_connection_policy_map = {
-    .type = BPF_MAP_TYPE_HASH,
-    .key_size = sizeof(connection_tuple_t),
-    .value_size = sizeof(uint32_t),
-    .max_entries = 1};
-
-SEC("maps")
 struct bpf_map_def egress_connection_policy_map = {
     .type = BPF_MAP_TYPE_HASH,
-    .key_size = sizeof(connection_tuple_t),
-    .value_size = sizeof(uint32_t),
-    .max_entries = 1};
+    .key_size = sizeof(destination_entry_t),
+    .value_size = sizeof(destination_entry_t),
+    .max_entries = 100};
+
+SEC("maps")
+struct bpf_map_def egress_statistics_map = {
+    .type = BPF_MAP_TYPE_ARRAY, .key_size = sizeof(uint32_t), .value_size = sizeof(uint32_t), .max_entries = 4};
+
+__inline void
+update_statistics(bool redirected, bool v4)
+{
+    uint32_t* count = NULL;
+    int key = 0;
+
+    if (!v4) {
+        key = 2;
+    }
+    if (redirected) {
+        key++;
+    }
+    count = bpf_map_lookup_elem(&egress_statistics_map, &key);
+    if (count != NULL) {
+        (*count) = (*count) + 1;
+    }
+}
 
 __inline int
 authorize_v4(bpf_sock_addr_t* ctx, struct bpf_map_def* connection_policy_map)
 {
-    connection_tuple_t tuple_key = {0};
-    int* verdict = NULL;
+    destination_entry_t tuple_key = {0};
+    destination_entry_t* verdict = NULL;
     int result;
+    bool redirected = false;
+    uint32_t destination_ip = bpf_ntohl(ctx->user_ip4);
 
-    tuple_key.dst_ip.ipv4 = ctx->user_ip4;
-    tuple_key.dst_port = ctx->user_port;
-    tuple_key.protocol = ctx->protocol;
+    tuple_key.destination_ip.ipv4 = ctx->user_ip4;
+    tuple_key.destination_port = ctx->user_port;
 
     verdict = bpf_map_lookup_elem(connection_policy_map, &tuple_key);
+    if (verdict == NULL) {
+        // Now lookup without destination port.
+        bpf_printk("entry not found 1 for %u:%u", destination_ip, ctx->user_port);
+        tuple_key.destination_port = 0;
+        verdict = bpf_map_lookup_elem(connection_policy_map, &tuple_key);
+    }
 
-    result = (verdict != NULL) ? *verdict : BPF_SOCK_ADDR_VERDICT_PROCEED;
-    bpf_printk("Returning verdict for v4 connect: %d", result);
+    if (verdict != NULL) {
+        bpf_printk("entry found for %u", destination_ip);
+        ctx->user_ip4 = verdict->destination_ip.ipv4;
+        if (tuple_key.destination_port != 0) {
+            ctx->user_port = verdict->destination_port;
+        }
+        redirected = true;
+    } else {
+        bpf_printk("entry not found 2 for %u", destination_ip);
+    }
+    update_statistics(redirected, true);
 
-    return result;
+    return BPF_SOCK_ADDR_VERDICT_PROCEED;
 }
 
 __inline int
 authorize_v6(bpf_sock_addr_t* ctx, struct bpf_map_def* connection_policy_map)
 {
-    connection_tuple_t tuple_key = {0};
-    int* verdict;
-    __builtin_memcpy(tuple_key.dst_ip.ipv6, ctx->user_ip6, sizeof(ctx->user_ip6));
-    tuple_key.dst_port = ctx->user_port;
-    tuple_key.protocol = ctx->protocol;
-
-    verdict = bpf_map_lookup_elem(connection_policy_map, &tuple_key);
-
-    return (verdict != NULL) ? *verdict : BPF_SOCK_ADDR_VERDICT_PROCEED;
+    update_statistics(false, false);
+    return BPF_SOCK_ADDR_VERDICT_PROCEED;
 }
 
 SEC("cgroup/connect4")
--- tools/port_quota/port_quota.cpp
+++ tools/port_quota/port_quota.cpp
@@ -4,14 +4,43 @@
 #include "bpf/bpf.h"
 #include "bpf/libbpf.h"
 #include "ebpf_api.h"
+#include "socket_helper.h"
 
 #include <windows.h>
 #include <io.h>
 #include <iostream>
 #include <string>
+#include <vector>
+
+#define htonl(x) _byteswap_ulong(x)
+#define htons(x) _byteswap_ushort(x)
+#define ntohl(x) _byteswap_ulong(x)
+#define ntohs(x) _byteswap_ushort(x)
 
 const char* authorize_v4_program = "authorize_connect4";
 const char* authorize_v6_program = "authorize_connect6";
+
+const char* egress_connection_policy_map = "egress_connection_policy_map";
+const char* egress_statistics_map = "egress_statistics_map";
+
+static std::string _add_operation("add");
+static std::string _delete_operation("delete");
+
+typedef struct _ip_address
+{
+    union
+    {
+        uint32_t ipv4;
+        uint32_t ipv6[4];
+    };
+} ip_address_t;
+
+typedef struct _destination_entry
+{
+    ip_address_t destination_ip;
+    uint16_t destination_port;
+    uint32_t protocol;
+} destination_entry_t;
 
 typedef struct _process_entry
 {
@@ -28,6 +57,9 @@ load(int argc, char** argv)
     bpf_program* v6_program = nullptr;
     bpf_link* v4_link = nullptr;
     bpf_link* v6_link = nullptr;
+    bpf_map* policy_map = nullptr;
+    bpf_map* stats_map = nullptr;
+
     UNREFERENCED_PARAMETER(argc);
     UNREFERENCED_PARAMETER(argv);
 
@@ -61,6 +93,18 @@ load(int argc, char** argv)
         return 1;
     }
 
+    policy_map = bpf_object__find_map_by_name(object, egress_connection_policy_map);
+    if (policy_map == nullptr) {
+        fprintf(stderr, "Failed to find %s map\n", egress_connection_policy_map);
+        return 1;
+    }
+
+    stats_map = bpf_object__find_map_by_name(object, egress_statistics_map);
+    if (policy_map == nullptr) {
+        fprintf(stderr, "Failed to find %s map\n", egress_statistics_map);
+        return 1;
+    }
+
     // Attach both the programs.
     result = ebpf_program_attach(v4_program, &EBPF_ATTACH_TYPE_CGROUP_INET4_CONNECT, nullptr, 0, &v4_link);
     if (result != ERROR_SUCCESS) {
@@ -87,6 +131,18 @@ load(int argc, char** argv)
     }
     fprintf(stdout, "Pinned v6 eBPF program to \"%s\"\n", authorize_v6_program);
 
+    // Pin both the maps.
+    if (bpf_obj_pin(bpf_map__fd(policy_map), egress_connection_policy_map) < 0) {
+        fprintf(stderr, "Failed to pin eBPF map [%s]: %d\n", egress_connection_policy_map, errno);
+        return 1;
+    }
+    fprintf(stdout, "Pinned eBPF map to \"%s\"\n", egress_connection_policy_map);
+    if (bpf_obj_pin(bpf_map__fd(stats_map), egress_statistics_map) < 0) {
+        fprintf(stderr, "Failed to pin eBPF map [%s]: %d\n", egress_statistics_map, errno);
+        return 1;
+    }
+    fprintf(stdout, "Pinned eBPF map to \"%s\"\n", egress_statistics_map);
+
     fprintf(stdout, "\nDone.\n");
     return 0;
 }
@@ -104,6 +160,7 @@ unload(int argc, char** argv)
         result = 1;
     }
     fprintf(stdout, "Unpinned v4 eBPF program from \"%s\"\n", authorize_v4_program);
+
     result = ebpf_object_unpin(authorize_v6_program);
     if (result != ERROR_SUCCESS) {
         fprintf(stderr, "Failed to unpin eBPF program from \"%s\": %d\n", authorize_v6_program, result);
@@ -111,8 +168,177 @@ unload(int argc, char** argv)
     }
     fprintf(stdout, "Unpinned v6 eBPF program from \"%s\"\n", authorize_v6_program);
 
+    result = ebpf_object_unpin(egress_connection_policy_map);
+    if (result != ERROR_SUCCESS) {
+        fprintf(stderr, "Failed to unpin eBPF map from \"%s\": %d\n", egress_connection_policy_map, result);
+        result = 1;
+    }
+    fprintf(stdout, "Unpinned eBPF map from \"%s\"\n", egress_connection_policy_map);
+
+    result = ebpf_object_unpin(egress_statistics_map);
+    if (result != ERROR_SUCCESS) {
+        fprintf(stderr, "Failed to unpin eBPF map from \"%s\": %d\n", egress_statistics_map, result);
+        result = 1;
+    }
+    fprintf(stdout, "Unpinned eBPF map from \"%s\"\n", egress_statistics_map);
+
     fprintf(stdout, "\nDone.\n");
     return result;
+}
+
+int
+stats(int argc, char** argv)
+{
+    uint32_t result;
+    fd_t map_fd;
+    uint32_t key = 0;
+    uint32_t v4_redirect = 0;
+    uint32_t v4_allow = 0;
+    uint32_t v6_redirect = 0;
+    uint32_t v6_allow = 0;
+
+    UNREFERENCED_PARAMETER(argc);
+    UNREFERENCED_PARAMETER(argv);
+
+    map_fd = bpf_obj_get((char*)egress_statistics_map);
+    if (map_fd == ebpf_fd_invalid) {
+        fprintf(stderr, "Failed to look up statistics map.\n");
+        return 1;
+    }
+
+    // Get v4 values.
+    result = bpf_map_lookup_elem(map_fd, &key, &v4_allow);
+    if (result != 0) {
+        fprintf(stderr, "Failed to read entry from stats map: %d\n", result);
+        return 1;
+    }
+    key++;
+    result = bpf_map_lookup_elem(map_fd, &key, &v4_redirect);
+    if (result != 0) {
+        fprintf(stderr, "Failed to read entry from stats map: %d\n", result);
+        return 1;
+    }
+
+    // Get v6 values.
+    key++;
+    result = bpf_map_lookup_elem(map_fd, &key, &v6_allow);
+    if (result != 0) {
+        fprintf(stderr, "Failed to read entry from stats map: %d\n", result);
+        return 1;
+    }
+    key++;
+    result = bpf_map_lookup_elem(map_fd, &key, &v6_redirect);
+    if (result != 0) {
+        fprintf(stderr, "Failed to read entry from stats map: %d\n", result);
+        return 1;
+    }
+
+    printf("STATISTICS:\n");
+    printf("  Allowed v4    : %d\n", v4_allow);
+    printf("  Allowed v6    : %d\n", v6_allow);
+    printf("  Redirected v4 : %d\n", v4_redirect);
+    printf("  Redirected v6 : %d\n\n", v6_redirect);
+
+    return 0;
+}
+
+int
+proxy(int argc, char** argv)
+{
+    struct sockaddr_storage destination_ip;
+    struct sockaddr_storage proxy_ip;
+    uint16_t destination_port;
+    uint16_t proxy_port;
+    destination_entry_t key = {0};
+    destination_entry_t value = {0};
+    ADDRESS_FAMILY family1, family2;
+    bool add = false;
+
+    WSAData data;
+
+    if (argc != 5) {
+        fprintf(stderr, "invalid parameters\n");
+        return 1;
+    }
+
+    std::string operation(argv[0]);
+
+    if (operation.compare(_add_operation) != 0 && operation.compare(_delete_operation) != 0) {
+        fprintf(stderr, "invalid operation: %s\n", operation.c_str());
+        return 1;
+    }
+    if (operation.compare(_add_operation) == 0) {
+        add = true;
+    }
+
+    int error = WSAStartup(2, &data);
+    if (error != 0) {
+        fprintf(stderr, "Unable to load Winsock: %d\n", error);
+        return 1;
+    }
+
+    std::string destination_ip_string(argv[1]);
+    std::string proxy_ip_string(argv[3]);
+
+    // printf("reached 1\n");
+
+    // printf("destination_ip_string = %s\n", destination_ip_string.c_str());
+    // printf("proxy_ip_string = %s\n", proxy_ip_string.c_str());
+
+    get_address_from_string(destination_ip_string, destination_ip, false, &family1);
+    get_address_from_string(proxy_ip_string, proxy_ip, false, &family2);
+
+    // printf("reached 2\n");
+
+    if (family1 != family2) {
+        fprintf(stderr, "Invalid parameter: Both addresses should be same family\n");
+        return 1;
+    }
+
+    destination_port = htons((uint16_t)atoi(argv[2]));
+    proxy_port = htons((uint16_t)atoi(argv[4]));
+
+    fd_t map_fd;
+    uint32_t result;
+
+    // printf("reached 3\n");
+
+    INET_SET_ADDRESS(family1, (PUCHAR)&key.destination_ip, INETADDR_ADDRESS((PSOCKADDR)&destination_ip));
+    INET_SET_ADDRESS(family1, (PUCHAR)&value.destination_ip, INETADDR_ADDRESS((PSOCKADDR)&proxy_ip));
+
+    key.destination_port = destination_port;
+    value.destination_port = proxy_port;
+
+    // printf("reached 4\n");
+
+    map_fd = bpf_obj_get((char*)egress_connection_policy_map);
+    if (map_fd == ebpf_fd_invalid) {
+        fprintf(stderr, "Failed to look up policy map.\n");
+        return 1;
+    }
+
+    if (add) {
+        result = bpf_map_update_elem(map_fd, &key, &value, EBPF_ANY);
+        if (result != EBPF_SUCCESS) {
+            fprintf(stderr, "Failed to add proxy entry: %d\n", result);
+            return 1;
+        }
+
+        fprintf(stdout, "Added proxy entry.\n");
+    } else {
+        result = bpf_map_delete_elem(map_fd, &key);
+        if (result != EBPF_SUCCESS) {
+            fprintf(stderr, "Failed to delete proxy entry: %d\n", result);
+            return 1;
+        }
+
+        fprintf(stdout, "Deleted proxy entry.\n");
+    }
+
+    _close(map_fd);
+    WSACleanup();
+
+    return 0;
 }
 
 typedef int (*operation_t)(int argc, char** argv);
@@ -122,8 +348,10 @@ struct
     const char* help;
     operation_t operation;
 } commands[]{
-    {"load", "load\tLoad the port quota eBPF program", load},
-    {"unload", "unload\tUnload the port quota eBPF program", unload}};
+    {"load", "load\tLoad the sock_addr eBPF program", load},
+    {"unload", "unload\tUnload the sock_addr eBPF program", unload},
+    {"stats", "stats\tPrint the stats for the eBPF program", stats},
+    {"proxy", "proxy {add|delete} dst_ip dst_port proxy_ip proxy_port\tConfigure proxy", proxy}};
 
 void
 print_usage(char* path)
