605145293a380b2f535c92723456cb48bc6b72d0
Anurag Saxena <anusa@microsoft.com>
2026-03-20 12:08:44 -0700

    array map lookup optimizations

--- tests/bpf2c_tests/expected/ambiguous_array_map_sys.c
+++ tests/bpf2c_tests/expected/ambiguous_array_map_sys.c
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 // Do not alter this generated file.
-// This file was generated from atomic_instruction_fetch_add.o
+// This file was generated from ambiguous_array_map.o
 
 #define NO_CRT
 #include "bpf2c.h"
@@ -15,7 +15,7 @@ DRIVER_INITIALIZE DriverEntry;
 DRIVER_UNLOAD DriverUnload;
 RTL_QUERY_REGISTRY_ROUTINE static _bpf2c_query_registry_routine;
 
-#define metadata_table atomic_instruction_fetch_add##_metadata_table
+#define metadata_table ambiguous_array_map##_metadata_table
 
 static GUID _bpf2c_npi_id = {/* c847aac8-a6f2-4b53-aea3-f4a94b9a80cb */
                              0xc847aac8,
@@ -187,7 +187,25 @@ static map_entry_t _maps[] = {
          13,                 // Identifier for a map template.
          0,                  // The id of the inner map template.
      },
-     "map"},
+     "map_a"},
+    {
+     {0, 0},
+     {
+         1,                  // Current Version.
+         80,                 // Struct size up to the last field.
+         80,                 // Total struct size including padding.
+     },
+     {
+         BPF_MAP_TYPE_ARRAY, // Type of map.
+         4,                  // Size in bytes of a map key.
+         8,                  // Size in bytes of a map value.
+         1,                  // Maximum number of entries allowed in the map.
+         0,                  // Inner map index.
+         LIBBPF_PIN_NONE,    // Pinning type for the map.
+         15,                 // Identifier for a map template.
+         0,                  // The id of the inner map template.
+     },
+     "map_b"},
 };
 #pragma data_seg(pop)
 
@@ -195,7 +213,7 @@ static void
 _get_maps(_Outptr_result_buffer_maybenull_(*count) map_entry_t** maps, _Out_ size_t* count)
 {
     *maps = _maps;
-    *count = 1;
+    *count = 2;
 }
 
 static void
@@ -207,7 +225,7 @@ _get_global_variable_sections(
     *count = 0;
 }
 
-static helper_function_entry_t func_helpers[] = {
+static helper_function_entry_t ambiguous_map_lookup_helpers[] = {
     {
      {1, 40, 40}, // Version header.
      1,
@@ -215,94 +233,105 @@ static helper_function_entry_t func_helpers[] = {
     },
 };
 
-static GUID func_program_type_guid = {0xf788ef4a, 0x207d, 0x4dc3, {0x85, 0xcf, 0x0f, 0x2e, 0xa1, 0x07, 0x21, 0x3c}};
-static GUID func_attach_type_guid = {0xf788ef4b, 0x207d, 0x4dc3, {0x85, 0xcf, 0x0f, 0x2e, 0xa1, 0x07, 0x21, 0x3c}};
-static uint16_t func_maps[] = {
+static GUID ambiguous_map_lookup_program_type_guid = {
+    0x608c517c, 0x6c52, 0x4a26, {0xb6, 0x77, 0xbb, 0x1c, 0x34, 0x42, 0x5a, 0xdf}};
+static GUID ambiguous_map_lookup_attach_type_guid = {
+    0xb9707e04, 0x8127, 0x4c72, {0x83, 0x3e, 0x05, 0xb1, 0xfb, 0x43, 0x94, 0x96}};
+static uint16_t ambiguous_map_lookup_maps[] = {
     0,
+    1,
 };
 
-#pragma code_seg(push, "sample~1")
+#pragma code_seg(push, "bind")
 static uint64_t
-func(void* context, const program_runtime_context_t* runtime_context)
-#line 25 "sample/undocked/atomic_instruction_fetch_add.c"
+ambiguous_map_lookup(void* context, const program_runtime_context_t* runtime_context)
+#line 30 "sample/ambiguous_array_map.c"
 {
-#line 25 "sample/undocked/atomic_instruction_fetch_add.c"
+#line 30 "sample/ambiguous_array_map.c"
     // Prologue.
-#line 25 "sample/undocked/atomic_instruction_fetch_add.c"
+#line 30 "sample/ambiguous_array_map.c"
     uint64_t stack[(UBPF_STACK_SIZE + 7) / 8];
-#line 25 "sample/undocked/atomic_instruction_fetch_add.c"
+#line 30 "sample/ambiguous_array_map.c"
     register uint64_t r0 = 0;
-#line 25 "sample/undocked/atomic_instruction_fetch_add.c"
+#line 30 "sample/ambiguous_array_map.c"
     register uint64_t r1 = 0;
-#line 25 "sample/undocked/atomic_instruction_fetch_add.c"
+#line 30 "sample/ambiguous_array_map.c"
     register uint64_t r2 = 0;
-#line 25 "sample/undocked/atomic_instruction_fetch_add.c"
+#line 30 "sample/ambiguous_array_map.c"
+    register uint64_t r3 = 0;
+#line 30 "sample/ambiguous_array_map.c"
+    register uint64_t r4 = 0;
+#line 30 "sample/ambiguous_array_map.c"
+    register uint64_t r5 = 0;
+#line 30 "sample/ambiguous_array_map.c"
+    register uint64_t r6 = 0;
+#line 30 "sample/ambiguous_array_map.c"
     register uint64_t r10 = 0;
 
-#line 25 "sample/undocked/atomic_instruction_fetch_add.c"
+#line 30 "sample/ambiguous_array_map.c"
     r1 = (uintptr_t)context;
-#line 25 "sample/undocked/atomic_instruction_fetch_add.c"
+#line 30 "sample/ambiguous_array_map.c"
     r10 = (uintptr_t)((uint8_t*)stack + sizeof(stack));
 
-    // EBPF_OP_MOV64_IMM pc=0 dst=r1 src=r0 offset=0 imm=0
-#line 25 "sample/undocked/atomic_instruction_fetch_add.c"
-    r1 = IMMEDIATE(0);
-    // EBPF_OP_STXW pc=1 dst=r10 src=r1 offset=-4 imm=0
-#line 27 "sample/undocked/atomic_instruction_fetch_add.c"
-    WRITE_ONCE_32(r10, (uint32_t)r1, OFFSET(-4));
-    // EBPF_OP_MOV64_REG pc=2 dst=r2 src=r10 offset=0 imm=0
-#line 27 "sample/undocked/atomic_instruction_fetch_add.c"
-    r2 = r10;
-    // EBPF_OP_ADD64_IMM pc=3 dst=r2 src=r0 offset=0 imm=-4
-#line 27 "sample/undocked/atomic_instruction_fetch_add.c"
-    r2 += IMMEDIATE(-4);
-    // EBPF_OP_LDDW pc=4 dst=r1 src=r1 offset=0 imm=1
-#line 28 "sample/undocked/atomic_instruction_fetch_add.c"
-    r1 = POINTER(runtime_context->map_data[0].address);
-    // EBPF_OP_CALL pc=6 dst=r0 src=r0 offset=0 imm=1
-#line 28 "sample/undocked/atomic_instruction_fetch_add.c"
-    {
-#line 28 "sample/undocked/atomic_instruction_fetch_add.c"
-        uint32_t _array_key = *(uint32_t*)(uintptr_t)r2;
-#line 28 "sample/undocked/atomic_instruction_fetch_add.c"
-        if (_array_key < 1) {
-#line 28 "sample/undocked/atomic_instruction_fetch_add.c"
-            r0 = (uint64_t)(uintptr_t)(runtime_context->map_data[0].array_data + (uint64_t)_array_key * 8);
-#line 28 "sample/undocked/atomic_instruction_fetch_add.c"
-        } else {
-#line 28 "sample/undocked/atomic_instruction_fetch_add.c"
-            r0 = 0;
-#line 28 "sample/undocked/atomic_instruction_fetch_add.c"
-        }
-#line 28 "sample/undocked/atomic_instruction_fetch_add.c"
-    }
-#line 28 "sample/undocked/atomic_instruction_fetch_add.c"
-    if ((runtime_context->helper_data[0].tail_call) && (r0 == 0)) {
-#line 28 "sample/undocked/atomic_instruction_fetch_add.c"
-        return 0;
-#line 28 "sample/undocked/atomic_instruction_fetch_add.c"
-    }
-    // EBPF_OP_JEQ_IMM pc=7 dst=r0 src=r0 offset=2 imm=0
-#line 29 "sample/undocked/atomic_instruction_fetch_add.c"
-    if (r0 == IMMEDIATE(0)) {
-#line 29 "sample/undocked/atomic_instruction_fetch_add.c"
+    // EBPF_OP_MOV64_IMM pc=0 dst=r6 src=r0 offset=0 imm=0
+#line 30 "sample/ambiguous_array_map.c"
+    r6 = IMMEDIATE(0);
+    // EBPF_OP_STXW pc=1 dst=r10 src=r6 offset=-4 imm=0
+#line 32 "sample/ambiguous_array_map.c"
+    WRITE_ONCE_32(r10, (uint32_t)r6, OFFSET(-4));
+    // EBPF_OP_LDXDW pc=2 dst=r2 src=r1 offset=16 imm=0
+#line 38 "sample/ambiguous_array_map.c"
+    READ_ONCE_64(r2, r1, OFFSET(16));
+    // EBPF_OP_AND64_IMM pc=3 dst=r2 src=r0 offset=0 imm=1
+#line 38 "sample/ambiguous_array_map.c"
+    r2 &= IMMEDIATE(1);
+    // EBPF_OP_LDDW pc=4 dst=r1 src=r1 offset=0 imm=2
+#line 38 "sample/ambiguous_array_map.c"
+    r1 = POINTER(runtime_context->map_data[1].address);
+    // EBPF_OP_JEQ_IMM pc=6 dst=r2 src=r0 offset=2 imm=0
+#line 38 "sample/ambiguous_array_map.c"
+    if (r2 == IMMEDIATE(0)) {
+#line 38 "sample/ambiguous_array_map.c"
         goto label_1;
-#line 29 "sample/undocked/atomic_instruction_fetch_add.c"
+#line 38 "sample/ambiguous_array_map.c"
     }
-    // EBPF_OP_MOV64_IMM pc=8 dst=r1 src=r0 offset=0 imm=1
-#line 29 "sample/undocked/atomic_instruction_fetch_add.c"
-    r1 = IMMEDIATE(1);
-    // EBPF_OP_ATOMIC64_ADD pc=9 dst=r0 src=r1 offset=0 imm=0
-#line 30 "sample/undocked/atomic_instruction_fetch_add.c"
-    InterlockedExchangeAdd64((volatile int64_t*)(uintptr_t)(r0 + OFFSET(0)), (uint64_t)r1);
+    // EBPF_OP_LDDW pc=7 dst=r1 src=r1 offset=0 imm=1
+#line 38 "sample/ambiguous_array_map.c"
+    r1 = POINTER(runtime_context->map_data[0].address);
 label_1:
-    // EBPF_OP_MOV64_IMM pc=10 dst=r0 src=r0 offset=0 imm=0
-#line 32 "sample/undocked/atomic_instruction_fetch_add.c"
-    r0 = IMMEDIATE(0);
-    // EBPF_OP_EXIT pc=11 dst=r0 src=r0 offset=0 imm=0
-#line 32 "sample/undocked/atomic_instruction_fetch_add.c"
+    // EBPF_OP_MOV64_REG pc=9 dst=r2 src=r10 offset=0 imm=0
+#line 38 "sample/ambiguous_array_map.c"
+    r2 = r10;
+    // EBPF_OP_ADD64_IMM pc=10 dst=r2 src=r0 offset=0 imm=-4
+#line 38 "sample/ambiguous_array_map.c"
+    r2 += IMMEDIATE(-4);
+    // EBPF_OP_CALL pc=11 dst=r0 src=r0 offset=0 imm=1
+#line 44 "sample/ambiguous_array_map.c"
+    r0 = runtime_context->helper_data[0].address(r1, r2, r3, r4, r5, context);
+#line 44 "sample/ambiguous_array_map.c"
+    if ((runtime_context->helper_data[0].tail_call) && (r0 == 0)) {
+#line 44 "sample/ambiguous_array_map.c"
+        return 0;
+#line 44 "sample/ambiguous_array_map.c"
+    }
+    // EBPF_OP_JEQ_IMM pc=12 dst=r0 src=r0 offset=1 imm=0
+#line 45 "sample/ambiguous_array_map.c"
+    if (r0 == IMMEDIATE(0)) {
+#line 45 "sample/ambiguous_array_map.c"
+        goto label_2;
+#line 45 "sample/ambiguous_array_map.c"
+    }
+    // EBPF_OP_LDXDW pc=13 dst=r6 src=r0 offset=0 imm=0
+#line 46 "sample/ambiguous_array_map.c"
+    READ_ONCE_64(r6, r0, OFFSET(0));
+label_2:
+    // EBPF_OP_MOV64_REG pc=14 dst=r0 src=r6 offset=0 imm=0
+#line 49 "sample/ambiguous_array_map.c"
+    r0 = r6;
+    // EBPF_OP_EXIT pc=15 dst=r0 src=r0 offset=0 imm=0
+#line 49 "sample/ambiguous_array_map.c"
     return r0;
-#line 25 "sample/undocked/atomic_instruction_fetch_add.c"
+#line 30 "sample/ambiguous_array_map.c"
 }
 #pragma code_seg(pop)
 #line __LINE__ __FILE__
@@ -312,17 +341,17 @@ static program_entry_t _programs[] = {
     {
         0,
         {1, 144, 144}, // Version header.
-        func,
-        "sample~1",
-        "sample_ext",
-        "func",
-        func_maps,
+        ambiguous_map_lookup,
+        "bind",
+        "bind",
+        "ambiguous_map_lookup",
+        ambiguous_map_lookup_maps,
+        2,
+        ambiguous_map_lookup_helpers,
         1,
-        func_helpers,
-        1,
-        12,
-        &func_program_type_guid,
-        &func_attach_type_guid,
+        16,
+        &ambiguous_map_lookup_program_type_guid,
+        &ambiguous_map_lookup_attach_type_guid,
     },
 };
 #pragma data_seg(pop)
@@ -338,7 +367,7 @@ static void
 _get_version(_Out_ bpf2c_version_t* version)
 {
     version->major = 1;
-    version->minor = 2;
+    version->minor = 1;
     version->revision = 0;
 }
 
@@ -349,7 +378,7 @@ _get_map_initial_values(_Outptr_result_buffer_(*count) map_initial_values_t** ma
     *count = 0;
 }
 
-metadata_table_t atomic_instruction_fetch_add_metadata_table = {
+metadata_table_t ambiguous_array_map_metadata_table = {
     sizeof(metadata_table_t),
     _get_programs,
     _get_maps,
